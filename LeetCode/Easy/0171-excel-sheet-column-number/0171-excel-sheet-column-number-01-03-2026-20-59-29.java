4d0f1345e80269c05de66d2fcf94a521029132f7
JaeYoung Heo <89104369+heoj10272@users.noreply.github.com>
2026-01-03 20:59:29 +0900

    Time: 1 ms (88.74%), Space: 43.7 MB (16.13%) - LeetHub

---
+++ LeetCode/Easy/0171-excel-sheet-column-number/0171-excel-sheet-column-number-01-03-2026-20-59-29.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int titleToNumber(String s) {
+        long r = 0;
+        for (int i = 0; i < s.length(); i++){
+            r = r * 26 + (s.charAt(i) - 'A' + 1);
+        }
+        return (int) r;
+    }
+}
